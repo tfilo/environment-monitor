450258055358de21869d16b13b8865bd1823f311
Tomáš Filo <tfilosk@gmail.com>
2020-04-27 09:11:09 +0200

    zmeny

--- environment_monitor.ino
+++ environment_monitor.ino
@@ -1,5 +1,7 @@
 #include "Wire.h"
 #include "SPI.h"
+#include "LowPower.h"
+
 #include "Adafruit_CCS811.h"
 #include "Adafruit_Si7021.h"
 #include "Adafruit_BMP280.h"
@@ -7,43 +9,45 @@
 #include "SSD1306Ascii.h"
 #include "SSD1306AsciiAvrI2c.h"
 
-#include "LowPower.h"
+#define OLED_I2C_ADDRESS 0x3C
 
-#define I2C_ADDRESS 0x3C
-
-SSD1306AsciiAvrI2c oled;
 Adafruit_CCS811 ccs;
 Adafruit_Si7021 si = Adafruit_Si7021();
 Adafruit_BMP280 bmp;
+SSD1306AsciiAvrI2c oled;
 
-void setup() {  
+float const static ACTUAL_ALTITUDE = 425.0F;
+
+void setup() {
+  ADCSRA = 0;  
   Serial.begin(115200);
 
-  if(!ccs.begin()){
+  if (!ccs.begin()) {
     Serial.println("Failed to start CCS811 sensor! Please check your wiring.");
     while(1);
   }
 
+  ccs.setDriveMode(CCS811_DRIVE_MODE_60SEC);
+
   if (!si.begin()) {
     Serial.println("Did not find Si7021 sensor!");
-    while (1);
+    while(1);
   }
 
   if (!bmp.begin(BMP280_ADDRESS_ALT, BMP280_CHIPID)) {
     Serial.println(F("Could not find a valid BMP280 sensor, check wiring!"));
-    while (1);
+    while(1);
   }
   
-  oled.begin(&Adafruit128x64, I2C_ADDRESS);
+  oled.begin(&Adafruit128x64, OLED_I2C_ADDRESS);
   oled.setFont(Adafruit5x7); 
   bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,      /* Operating Mode. */
-                  Adafruit_BMP280::SAMPLING_X2,      /* Temp. oversampling */
-                  Adafruit_BMP280::SAMPLING_X2,      /* Pressure oversampling */
-                  Adafruit_BMP280::FILTER_X8,        /* Filtering. */
-                  Adafruit_BMP280::STANDBY_MS_4000); /* Standby time. */  
-  oled.clear();
+                  Adafruit_BMP280::SAMPLING_X1,      /* Temp. oversampling */
+                  Adafruit_BMP280::SAMPLING_X1,      /* Pressure oversampling */
+                  Adafruit_BMP280::FILTER_OFF,        /* Filtering. */
+                  Adafruit_BMP280::STANDBY_MS_4000); /* Standby time. */
 
-  ADCSRA = 0; 
+  oled.clear();
 }
 
 int counter = 100;
@@ -56,9 +60,9 @@ void loop() {
     float tempSI = si.readTemperature();
     float humSI = si.readHumidity();
     float tempBMP = bmp.readTemperature();
-    float pressBMP = bmp.readPressure();
-    float pressBMPalt = bmp.seaLevelForAltitude(425, pressBMP);
-
+    float pressBMP = bmp.readPressure() / 100.0F;
+    float pressBMPsea = bmp.seaLevelForAltitude(ACTUAL_ALTITUDE, pressBMP);
+    
     if (counter >= 100) {
       counter = 0;
       Serial.print("Updating enviromental data to ");
@@ -105,7 +109,7 @@ void loop() {
 
       oled.setRow(row++);
       oled.setCol(1);
-      oled.print("Temp BMP280: ");
+      oled.print("Temp BME280: ");
       oled.print(tempBMP);
       oled.print("C"); 
       oled.clearToEOL ();
@@ -113,7 +117,7 @@ void loop() {
       oled.setRow(row++);
       oled.setCol(1);
       oled.print("Pres BMP280: ");
-      oled.print(pressBMPalt / 100, 1);
+      oled.print(pressBMPsea, 1);
       oled.print("hPa"); 
       oled.clearToEOL ();
 
@@ -130,12 +134,12 @@ void loop() {
       Serial.print(" TEMP_BMP:");
       Serial.print(tempBMP);
       Serial.print(" PRESS_BMP:");
-      Serial.println(pressBMP / 100, 1);
+      Serial.println(pressBMP, 1);
     } else {
       Serial.println("ERROR!");
       while(1);
     }
   }
-  delay(10); 
+  delay(1); 
   LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
 }
